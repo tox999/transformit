7378148a6dc587b3fdbc9ee4e424e849fbb5d861
Tom Fanta <tom@fanta.work>
2022-08-08 15:59:14 +0200

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from posixpath import split
-import pprint
+from pprint import pprint
 from typing import Any, Tuple
 from urllib.parse import urlparse
 import requests
@@ -13,21 +13,22 @@ DND5_API_HOST = "https://www.dnd5eapi.co/"
 COMMANDS ={
     "exit" : 
     {
-        "func": close_app, 
+        "func": "close_app", 
         "alt_names": ["q","quit","exit","close"]
     },
     "hi" : 
     {
-        "func": hi, 
+        "func": "hi", 
         "alt_names": ["h","hey","hello"]
     },
 }
 
-def hi(*args, **kwargs)-> Tuple(str, bool):
-    return "hi", False
+class Engine:
+    def hi(*args, **kwargs)-> Tuple[str, bool]:
+        return "hi", False
 
-def close_app(*args, **kwargs) -> Tuple(str, bool):
-    return "bye", True
+    def close_app(*args, **kwargs) -> Tuple[str, bool]:
+        return "bye", True
 
 def get_secrets() -> dict:
     with open(SECRETS_FILE_PATH) as f:
@@ -40,23 +41,42 @@ def send_request(url, ep):
     response = requests.request("GET", url, headers=headers)
     print(response.text)
 
-def parse_user_cmd(user_cmd:str)->Tuple(str,Any,Any):
-    user_cmd.split(" ")
-    raise NotImplemented
+def parse_user_cmd(user_cmd:str, cmd_arg_sep=" ", arg_sep=" ", kwargs_sep="=")->Tuple[str,Any,Any]:
+    cmd_and_args = user_cmd.split(cmd_arg_sep)
+    args=[]
+    kwargs=dict()
+    cmd_name = cmd_and_args[0]
+
+    if len(cmd_and_args) > 1:
+        args_and_kwargs = cmd_and_args[1:].split(arg_sep)
+        args = [arg for arg in args_and_kwargs if kwargs_sep not in arg]
+        
+        for kwarg in args_and_kwargs:
+            if kwargs_sep in kwarg:
+                kwarg_name_and_value = kwarg.split(kwargs_sep)
+                kwarg_name = kwarg_name_and_value[0]
+                kwarg_value = None
+                if len(kwarg_name_and_value) > 1:
+                    kwarg_value = kwarg_name_and_value[1:]
+                kwargs[kwarg_name] = kwarg_value
+    
+    return cmd_name, args, kwargs
 
 
 def main(*args, **kwargs):
     url = kwargs["url"]
+    engine = Engine()
     should_close = False
+
     while(should_close == False):
-        user_cmd = input()
+        user_cmd = input("Your command?")
         
         cmd_name, args, kwargs = parse_user_cmd(user_cmd)
       
         alt_cmd_names = {item for sublist in [v["alt_names"] for _,v in COMMANDS.items()] for item in sublist}
-        cmd_names = COMMANDS.keys().union(alt_cmd_names)  
+        cmd_names = set(COMMANDS.keys()).union(alt_cmd_names)  
         if cmd_name not in cmd_names:
-            pprint(f"commands does not exists. Possible commands: {cmd_names}")
+            pprint(f"Commands does not exist. Possible commands: {cmd_names}")
             continue
         
         for cmd_lib_name, cmd_details in COMMANDS.items():
@@ -65,8 +85,12 @@ def main(*args, **kwargs):
                     cmd = cmd_lib_name
             else:
                 cmd = cmd_name
-
-        response, should_close = COMMANDS[cmd](*args, **kwargs)
+        try:
+            cmd_func = getattr(engine, cmd)
+            response, should_close = cmd_func(*args, **kwargs)
+        except AttributeError:
+            response = "Cannot execute - not implemented."
+        
         pprint(response)
 
 
